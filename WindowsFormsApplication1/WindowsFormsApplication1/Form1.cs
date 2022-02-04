12fc8853d6ab721cab7ae35b6328e6a31a7b6095
zjpiraman2018 <57293196+zjpiraman2018@users.noreply.github.com>
2022-02-04 19:28:45 +0800

    convert pdf file to tiff

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+
+            string[] docs = Directory.GetFiles(textBox1.Text, "*.pdf");
+            int rec = 0;
+            foreach (var item in docs)
+            {
+                rec += 1;
+                Rasterizer r = new Rasterizer();
+                r.totalDocs = docs.Length;
+                r.currentIndex = rec;
+                r.Form = this;
+                r.pageFrom = 1;
+                r.pageTo = 100;
+                r.inputFile = item;
+                r.outputFile = Path.Combine(Path.GetDirectoryName(item) , "page-%03d.png");
+                r.FileName = Path.GetFileNameWithoutExtension(item);
+                r.Start();
+            }
+
+
+            button1.Enabled = true;
+            MessageBox.Show("Done!");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox1.Text = folderBrowserDialog1.SelectedPath;
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Rasterizer.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ghostscript.NET.Processor;
+using System.IO;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using System.Text.RegularExpressions;
+using System.Drawing.Imaging;
+using System.Net;
+
+namespace WindowsFormsApplication1
+{
+    class Rasterizer
+    {
+        public Form1 Form { get; set; }
+        public string FileName { get; set; }
+        public string inputFile { get; set; }
+        public string outputFile { get; set; }
+        public int pageFrom { get; set; }
+        public int pageTo { get; set; }
+        private int totalPage { get; set; }
+        public int currentIndex { get; set; }
+        public int totalDocs { get; set; }
+
+
+        public void Start()
+        {
+
+            this.totalPage = GetNoOfPagesPDF(inputFile);
+
+            using (GhostscriptProcessor ghostscript = new GhostscriptProcessor())
+            {
+                ghostscript.Processing += new GhostscriptProcessorProcessingEventHandler(ghostscript_Processing);
+                ghostscript.Completed += Ghostscript_Completed;
+                List<string> switches = new List<string>();
+                switches.Add("-empty");
+                switches.Add("-dSAFER");
+                switches.Add("-dBATCH");
+                switches.Add("-dNOPAUSE");
+                switches.Add("-dNOPROMPT");
+                switches.Add("-dFirstPage=" + pageFrom.ToString());
+                switches.Add("-dLastPage=" + pageTo.ToString());
+                switches.Add("-sDEVICE=png16m");
+                switches.Add("-r150");
+                switches.Add("-dTextAlphaBits=4");
+                switches.Add("-dGraphicsAlphaBits=4");
+                switches.Add(@"-sOutputFile=" + outputFile);
+                switches.Add(@"-f");
+                switches.Add(inputFile);
+
+                ghostscript.Process(switches.ToArray());
+            }
+        }
+
+        private void Ghostscript_Completed(object sender, GhostscriptProcessorEventArgs e)
+        {
+            MergeImage(Path.GetDirectoryName(inputFile));
+        }
+
+        void ghostscript_Processing(object sender, GhostscriptProcessorProcessingEventArgs e)
+        {
+            this.Form.Text = "Converted: " + currentIndex.ToString() + " / Total Document: "  + totalDocs.ToString() + " / Processing: [" + this.FileName +"  ("+ ((e.CurrentPage.ToString() + "/" + totalPage.ToString())).ToString() + ")]" ;
+        }
+
+
+        public static int GetNoOfPagesPDF(string FileName)
+        {
+            int result = 0;
+            FileStream fs = new FileStream(FileName, FileMode.Open, FileAccess.Read);
+            StreamReader r = new StreamReader(fs);
+            string pdfText = r.ReadToEnd();
+
+            System.Text.RegularExpressions.Regex regx = new Regex(@"/Type\s*/Page[^s]");
+            System.Text.RegularExpressions.MatchCollection matches = regx.Matches(pdfText);
+            result = matches.Count;
+            return result;
+
+        }
+
+        public bool isNumeric(string text)
+        {
+            Regex reNum = new Regex(@"^\d+$");
+            return reNum.Match(text).Success;
+        }
+
+        public string ConvertToTiff(string dir, string name) {
+            string tif = Path.Combine(dir,  name + ".tif");
+            string pngFile = Path.Combine(dir, "page-"+ ("000" + name).Substring(("000" + name).Length - 3, 3) + ".png");
+            var png = Image.FromFile(pngFile);
+            png.Save(tif, ImageFormat.Tiff);
+            png.Dispose();
+            png = null;
+            File.Delete(pngFile);
+            return tif;
+        }
+
+        public bool MergeImage(string Folder)
+        {
+            bool bRetval = false;
+            for (int i = 1; i <= this.totalPage; i++)
+            {
+                ConvertToTiff(Folder, i.ToString());
+            }
+
+            string[] aImg2 = Directory.GetFiles(Folder, "*.tif");
+            TiffBitmapEncoder encoder = new TiffBitmapEncoder();
+
+            using (FileStream stream = new FileStream(Path.Combine(Folder, this.FileName +  ".tif"), FileMode.OpenOrCreate))
+            {
+                foreach (var img in aImg2.Where(a => isNumeric(Path.GetFileNameWithoutExtension(a))))
+                {
+                    BitmapFrame frame = BitmapFrame.Create(new System.Uri(img), BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+                    encoder.Compression = TiffCompressOption.Lzw; // compress to 
+                    encoder.Frames.Add(frame);
+                }
+                encoder.Save(stream);
+            }
+
+            foreach (var img in aImg2.Where(a =>  isNumeric(Path.GetFileNameWithoutExtension(a))))
+            {
+                File.Delete(img);
+            }
+
+
+
+
+            return bRetval;
+
+        }
+
+
+    }
+
+
+
+}
